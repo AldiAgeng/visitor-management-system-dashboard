7e30b37f5ce40d10e477fac7e93e599c313b63cd
AldiAgeng <aldiageng48@gmail.com>
2025-07-06 18:01:36 +0700

    change url api prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  baseUrl: 'http://localhost:8000/api/v1'
+  baseUrl: 'http://aldiageng.xyz/v1/api',
 };
